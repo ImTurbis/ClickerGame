5e4a6ebea3ca3f0ac5563ca01f4052c8b2b07e7a
ElTurbinas <franciscortes50@gmail.com>
2022-04-26 17:43:05 -0500

    Vulnerabilidades parchadas

--- src/index.js
+++ src/index.js
@@ -3,39 +3,35 @@ const app = express()
 const path = require('path')
 const megadb = require('megadb')
 const bodyParser = require("body-parser");
+const fs = require('fs')
+const escape = require('escape-html');
 
+
+const RateLimit = require('express-rate-limit');
+const limiter = new RateLimit({
+    windowMs: 1*60*1000, // 1 minute
+    max: 100, // limit each IP to 100 requests per windowMs
+});
+
+app.use(limiter);
 app.use(bodyParser.json());
+
 app.all('/', function (req, res) {
     res.redirect(301, '/game')
 })
 
-app.get('/public/styles/index.css', function (req, res) {
-    res.sendFile(path.join(__dirname, '/public/styles/index.css'))
+//Aqui voy a tener que dar un aviso sobre el uso de la ip para guardar la partiva
+app.get('/game/advertencia', function (req, res) {
+    res.sendFile(path.join(__dirname + '/public/advertencia.html'))
 })
 
-app.get('/public/scripts/index.js', function (req, res) {
-    res.sendFile(path.join(__dirname, '/public/scripts/index.js'))
-})
-
-app.get('/private/styles/index.css', function (req, res) {
-    res.sendFile(path.join(__dirname, '/private/styles/index.css'))
-})
-
-app.get('/private/scripts/index.js', function (req, res) {
-    res.sendFile(path.join(__dirname, '/private/scripts/index.js'))
-})
-
-app.get('/private/view/index.html', function (req, res) {
-    res.sendFile(path.join(__dirname, '/private/view/index.html'))
+app.get('/private/:file', function (req, res) {
+    fs.existsSync(path.join(__dirname + '/private/' + req.params.file)) ? res.sendFile(path.join(__dirname + '/private/' + req.params.file)) : res.sendFile(path.join(__dirname + '/public/404.html'))
 })
 
 //Necesito insertar todos los archivos de la carpeta public de estilos y scripts que se llamen game
-app.get('/public/styles/game.css', function (req, res) {
-    res.sendFile(path.join(__dirname, '/public/styles/game.css'))
-})
-
-app.get('/public/scripts/game.js', function (req, res) {
-    res.sendFile(path.join(__dirname, '/public/scripts/game.js'))
+app.get('/public/:file', function (req, res) {
+    fs.existsSync(path.join(__dirname + '/public/' + req.params.file)) ? res.sendFile(path.join(__dirname + '/public/' + req.params.file)) : res.sendFile(path.join(__dirname + '/public/404.html'))
 })
 
 app.get('/game', function (req, res) {
@@ -46,17 +42,24 @@ app.get('/package.json', function (req, res) {
     res.sendFile(path.join(__dirname, '/../package.json'))
 })
 
-app.get('/api/game/:id', function (req, res) {
-    res
-    .status(200)
-    .send('[API] Se conecto con la id: ' + req.params.id)
+app.get('/api/game/:id', async function (req, res) {
+    const db = new megadb.crearDB('game')
+    const game = db.obtener(req.params.id)
+    if(!game) {
+        res.json({ error: 'No existe el juego' +  escape(req.params.id) })
+    } else {
+        res.status(200).json(game)
+    }
 })
 
-app.post('/api/game/:id', function (req, res) {
+app.post('/api/game/:id', function (req, res, next) {
+    
     const data = req.body;
-        var alias
+        var alias = '';
         if(!data.alias) {
             alias = `Player ${(Math.floor(Math.random() * 100) + 1).valueOf()}`
+        } else {
+            alias = data.alias
         }
     if(req.params.id === 'undefined') {
         res.status(404).send(JSON.stringify({ 
@@ -74,7 +77,7 @@ app.post('/api/game/:id', function (req, res) {
         const db = new megadb.crearDB('players_data', 'api_data')
         db.establecer(req.params.id, {
             points: data.points,
-            alias: data.alias
+            alias: alias
         })
     } catch (err) {
         res.status(500).send(JSON.stringify({ 
@@ -106,7 +109,6 @@ app.get('/api/scripts/index.js', function (req, res) {
     res.sendFile(path.join(__dirname, '/api/scripts/index.js'))
 })
 
-//oyp3hxs
 
 
 app.listen(1114)
